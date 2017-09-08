94c8c7d51f02441813248a0bd322fcb246fc52bc
Jean Cavallo <jean.cavallo@coopengo.com>
2017-09-08 15:25:48 +0200

    Complete homework

--- modules/library/library.py
+++ modules/library/library.py
@@ -2,10 +2,34 @@ from trytond.model import ModelSQL, ModelView
 
 
 __all__ = [
+    'Genre',
+    'Editor',
     'Author',
+    'Book',
+    'Exemplary',
     ]
+
+
+class Genre(ModelSQL, ModelView):
+    'Genre'
+    __name__ = 'library.genre'
+
+
+class Editor(ModelSQL, ModelView):
+    'Editor'
+    __name__ = 'library.editor'
 
 
 class Author(ModelSQL, ModelView):
     'Author'
     __name__ = 'library.author'
+
+
+class Book(ModelSQL, ModelView):
+    'Book'
+    __name__ = 'library.book'
+
+
+class Exemplary(ModelSQL, ModelView):
+    'Exemplary'
+    __name__ = 'library.book.exemplary'
