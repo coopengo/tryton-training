87a61864e4e4a3a737f006f3908b0f2e28f57a0a
Jean Cavallo <jean.cavallo@coopengo.com>
2017-09-11 15:33:37 +0200

    Initial commit

--- modules/library/library.py
+++ modules/library/library.py
@@ -39,6 +39,8 @@ class Author(ModelSQL, ModelView):
     __name__ = 'library.author'
 
     books = fields.One2Many('library.book', 'author', 'Books')
+    name = fields.Char('Name', required=True)
+    gender = fields.Selection([('man', 'Man'), ('woman', 'Woman')], 'Gender')
 
 
 class Book(ModelSQL, ModelView):
@@ -53,6 +55,12 @@ class Book(ModelSQL, ModelView):
         required=False)
     editor = fields.Many2One('library.editor', 'Editor', ondelete='RESTRICT',
         required=True)
+    summary = fields.Text('Summary')
+    cover = fields.Binary('Cover')
+    page_count = fields.Integer('Page Count',
+        help='The number of page in the book')
+    edition_stopped = fields.Boolean('Edition stopped',
+        help='If True, this book will not be printed again in this version')
 
 
 class Exemplary(ModelSQL, ModelView):
@@ -61,3 +69,5 @@ class Exemplary(ModelSQL, ModelView):
 
     book = fields.Many2One('library.book', 'Book', ondelete='CASCADE',
         required=True)
+    acquisition_date = fields.Date('Acquisition Date')
+    acquisition_price = fields.Numeric('Acquisition Price', digits=(16, 2))
